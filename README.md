435cea23ac1739d197ecde4d6e8edd45b7adee95
JoviTech <jovitechgaropaba@gmail.com>
2024-08-01 16:21:09 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <img src="protótipo.png" alt="Demonstração do projeto" width="100%" />
+</p>
+
+## 🖥️ Projeto
+Esse é um projeto que controla os semáforos de 4 vias e 4 faixas de pedestres.
+
+## 🚀 Tecnologias
+Esse projeto foi desenvolvido durante o curso de Engenharia de Computação com as seguintes tecnologias:
+
+- Arduino
+- Github
+
+## 🏷️ LAYOUT
+Você pode visualizar o layout do projeto através 
+[desse link](https://wokwi.com/projects/402327876478794753).
--- sketch.ino
+++ sketch.ino
@@ -1,42 +1,9 @@
-#define S1VERDE 23
-#define S1AMARELO 25
-#define S1VERMELHO 27
-
-#define S2VERDE 29
-#define S2AMARELO 31
-#define S2VERMELHO 33
-
-#define S3VERDE 35
-#define S3AMARELO 37
-#define S3VERMELHO 39
-
-#define S4VERDE 41
-#define S4AMARELO 43
-#define S4VERMELHO 45
-
-#define P1VERDE A8
-#define P1VERMELHO A9
-
-#define P2VERDE A10
-#define P2VERMELHO A11
-
-#define P3VERDE A12
-#define P3VERMELHO A13
-
-#define P4VERDE A14
-#define P4VERMELHO A15
-
-#define BTN1 3
-#define BTN2 2
-#define BTN3 20
-#define BTN4 21
+#include<config.h>
 
 volatile bool pedestreSolicitadoNorte = false;
 volatile bool pedestreSolicitadoLeste = false;
 volatile bool pedestreSolicitadoSul = false;
 volatile bool pedestreSolicitadoOeste = false;
-
-
 
 void setup() {
   pinMode(S1VERDE, OUTPUT);
